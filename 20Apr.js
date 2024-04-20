6293da2df040db8a36d5614a610e40eee0bf4540
maadii01 <154726440+maadii01@users.noreply.github.com>
2024-04-20 13:56:28 +0500

    Add files via upload

---
+++ 20Apr.js
@@ -0,0 +1,64 @@
+function number(){
+    let num=parseInt(prompt("Enter Your Number"));
+    let numrev=num.toString().split('').reverse().join('');
+    document.write("This is the reverse order of the number====>"+numrev);
+
+}
+
+// function getVowels(string) {
+//     let Vowels = "aAeEiIoOuU";
+//     let vow=prompt("Enter Your Word");
+//     let vowelsCount = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         if (Vowels.indexOf(string[i]) !== -1) {
+//             vowelsCount += 1;
+//         }
+//     }
+//     return vowelsCount;
+//     document.write("This is the number of the vowels in the word you entered"+vowelsCount);
+// }
+
+// function getVowels(string) {
+//     let Vowels = "aAeEiIoOuU";
+//     let vowelsCount = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         if (Vowels.indexOf(string[i]) !== -1) {
+//             vowelsCount += 1;
+//         }
+//     }
+//     return vowelsCount;
+// }
+
+
+// console.log(
+//     "The Number of vowels in -" +
+//         " hammad khalid" +
+//         getVowels(
+//             "hammad khalid"
+//         )
+// );
+
+function vow(){
+let str=prompt("Enter Your Word");
+let counter=0;
+let vowels=['a','e','i','o','u'];
+for(ch of str)
+{
+    if (vowels.includes(ch)){
+        counter++;
+    }
+}
+document.getElementById("count").innerHTML=counter;
+// document.write(counter);
+}
+
+function form(){
+    let firstname=document.getElementById("fname").value;
+    let lastname=document.getElementById("lname").value;
+    let password=document.getElementById("pass").value;
+
+    localStorage.setItem("First Name",firstname);
+localStorage.setItem("Last Name",lastname);
+localStorage.setItem("Password",password);
+
+}
